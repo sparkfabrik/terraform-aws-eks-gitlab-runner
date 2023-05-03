f77c59da2c06b587c66554e41d6f52877e31bf4a
Filippo Merante <filippo.merante@sparkfabrik.com>
2023-05-03 15:52:34 +0200

    refs #2143: Remove gitlab external user runner

--- examples/main.tf
+++ examples/main.tf
@@ -2,5 +2,4 @@ module "gitlab_runner" {
   source                    = "../"
   runner_registration_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   eks_cluster_name          = "test-eks-cluster"
-  add_external_runner_user  = true
 }
